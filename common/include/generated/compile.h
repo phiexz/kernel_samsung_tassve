ce2c17966ffe0ca4970d28272da5148e93d4859c
Andika Edo <dhiika@phiexz.com>
2012-07-26 02:47:31 +0700

    Initial Commit with some changes: - Use LZMA on kernel & Ramdisk to save space - Force bad vermagic modules - Indentified "px-kernel-base" - Version = date compiled - And more..

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 11 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#build-20120726 PREEMPT Thu Jul 26 02:20:38 WIT 2012"
+#define LINUX_COMPILE_TIME "02:20:38"
+#define LINUX_COMPILE_BY "dhiika"
+#define LINUX_COMPILE_HOST "phiexz"
+#define LINUX_COMPILE_DOMAIN "localdomain"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
